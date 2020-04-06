fdc6eb1c1b41d7bb427179dfeffc5b2c8062cbff
Wolf McNally <wolf@wolfmcnally.com>
2020-04-06 00:35:01 -0700

    Working on option parsing.

--- src/params.c
+++ src/params.c
@@ -14,6 +14,7 @@
 #include "utils.h"
 #include "input.h"
 #include "output.h"
+#include "random.h"
 
 params* new_params() {
     params* p = calloc(sizeof(params), 1);
@@ -56,10 +57,67 @@ void dispose_params(params* p) {
     free(p);
 }
 
-char doc[] = "A program with a help and default options";
-struct argp argp = { NULL, NULL, NULL, doc };
+static int parse_opt(int key, char* arg, struct argp_state* state) {
+    int* arg_count = state->input;
+    switch (key) {
+        case 'i': {
+            printf("in: %s\n", arg);
+        }
+        break;
+        case 'o': {
+            printf("out: %s\n", arg);
+        }
+        break;
+        case 'c': {
+            printf("count: %s\n", arg);
+        }
+        break;
+        case 1000: {
+            printf("low: %s\n", arg);
+        }
+        break;
+        case 1001: {
+            printf("high: %s\n", arg);
+        }
+        break;
+        case ARGP_KEY_INIT: {
+            printf("INIT\n");
+        }
+        break;
+        case ARGP_KEY_ARG: {
+            printf("arg: %s\n", arg);
+        }
+        break;
+        case ARGP_KEY_END: {
+            printf("END\n");
+        }
+        break;
+    }
+    return 0;
+}
+
+struct argp_option options[] = {
+    {"in", 'i', "[random|hex|bits|cards|dice|base6|base10|ints|bip39|slip39]", 0, "Specify the input format (default: random)"},
+    {"out", 'o', "[hex|bits|cards|dice|base6|base10|ints|bip39|slip39]", 0, "Specify the output format (default: hex)."},
+    {"count", 'c', "N", 0, "Specify the count of output units (default: 32)"},
+    {"deterministic", 'd', "SEED", 0, "Use a deterministic random number generator with SEED"},
+    {0, 0, 0, 0, "ints Input and Output Options:", 7},
+    {"low", 1000, "LOW", 0, "The lowest int returned (default: 1)"},
+    {"high", 1001, "HIGH", 0, "The highest int returned (default: 9)"},
+    {0, 0, 0, 0, "SLIP39 Output Options:", 6},
+    {"group-threshold", 1002, "N", 0, "The number of groups that must contribute (default: 1)"},
+    {"group", 1003, "N-of-M", 0, "The group specification (default: 1-of-1)"},
+    { 0 }
+};
+
+const char* argp_program_version = "version 1.0";
+const char* argp_program_bug_address = "who@dunnit.com";
+
+char doc[] = "Converts cryptographic seeds between various forms.";
+struct argp argp = { options, parse_opt, "WORD [WORD [WORD [WORD]]]", doc };
+
 params* parse_params( int argc, char *argv[] ) {
-    argp_parse( &argp, argc, argv, 0, 0, NULL );
     params* p = new_params();
+    argp_parse( &argp, argc, argv, 0, 0, p );
     return p;
 }
